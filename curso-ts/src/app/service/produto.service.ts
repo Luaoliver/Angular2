5defe5abe13030a4795df413b2b07707e410ed96
luaoliver <lulu-so@hotmail.com>
2021-04-30 15:11:15 -0300

    called rest api

--- curso-ts/src/app/service/produto.service.ts
+++ curso-ts/src/app/service/produto.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; //responsavel por fazer consultas em APIs
-import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { HttpClient } from '@angular/common/http'; //responsavel por fazer consultas em APIs e já faz a conversão de json para objeto
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  constructor() { }
+  constructor(private $http: HttpClient) { }
   
   listar(){
-    return [ 'Computador', 'Teclado', 'Fones', 'Mouse' ]
+    return this.$http.get('http://localhost:3000/produtos')
   }
   
 }
