d49ab51643fe0f548c3995f20ae368361689980b
luaoliver <lulu-so@hotmail.com>
2021-04-30 11:16:39 -0300

    knowing types of variables

--- curso-ts/src/app/cadastro/cadastro.component.ts
+++ curso-ts/src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import {DiasDaSemana} from './../dias-da-semana.enum';
+
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
@@ -9,6 +11,10 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class CadastroComponent implements OnInit {
 
   id: any
+  texto: string = 'Teste' //variavel string
+  valor: number = 0 //variável number
+  endereco: [string, number] = ['Rua teste numero: ', 4] //tuple array
+  dia: DiasDaSemana = DiasDaSemana.qui
 
   constructor(
     private route: ActivatedRoute,
@@ -20,7 +26,6 @@ export class CadastroComponent implements OnInit {
     this.route.params.subscribe(parametros => {
       if(parametros['id']){
         this.id = parametros['id']
-        alert(this.id)
       }
     })
   }
